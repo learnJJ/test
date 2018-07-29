9c686254ebbd3eacdc5584b7503f5ae16a7a7de8
Gaargi <Gaargi@Gaargi-PC>
2018-07-29 23:05:10 +0530

    test welocm epage

---
+++ SampleGit/src/welcomePage.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author 
+ *
+ */
+public class welcomePage {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welocme to git programming");
+	}
+
+}
